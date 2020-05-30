a7308143b762bfac26e303ffdd649101e70ad3dd
neha6185 <neha.6185@gmail.com>
2020-05-30 11:13:45 -0700

    First Assignment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,46 @@
 import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
+import About from "./AboutComponent";
+import Contact from "./ContactComponent";
 import { View, Platform } from "react-native";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import { createStackNavigator, createDrawerNavigator } from "react-navigation";
+
+const ContactNavigator = createStackNavigator(
+  {
+    Contact: { screen: Contact },
+  },
+  {
+    initialRouteName: "Contact",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+const AboutNavigator = createStackNavigator(
+  {
+    About: { screen: About },
+  },
+  {
+    initialRouteName: "About",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -45,6 +82,8 @@ const MainNavigator = createDrawerNavigator(
   {
     Home: { screen: HomeNavigator },
     Directory: { screen: DirectoryNavigator },
+    About: { screen: AboutNavigator },
+    Contact: { screen: ContactNavigator },
   },
   {
     drawerBackgroundColor: "#CEC8FF",
